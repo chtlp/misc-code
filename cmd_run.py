8d3b018f1b39954341d3ae7597a954c78eb75b4c
Linpeng Tang <chnttlp@gmail.com>
2013-01-03 19:50:29 -0500

    cmd_run

---
+++ cmd_run.py
@@ -0,0 +1,35 @@
+from string import Template
+import subprocess
+import inspect
+
+'''
+run a command line using bash -c
+ns: the namspace or dictionary object
+'''
+def cmd_run(cmd, ns=None):
+    d = {}
+    fr = inspect.currentframe().f_back
+    d.update(fr.f_locals)
+    d.update(fr.f_globals)
+
+    if ns is None:
+        pass
+    elif '__getitem__' in dir(ns):
+        d.update(ns)
+    else:
+        d.update(map(lambda k: (k, getattr(ns, k)), dir(ns)))
+
+    cmd = Template(cmd.strip()).substitute(d)
+    # print cmd
+    return subprocess.call(['bash', '-exc', cmd])        
+
+
+def _test():
+    class Variables:
+        msg = "world"
+
+    cmd_run('echo $msg $msg2', Variables)
+
+if __name__ == '__main__':
+    msg2 = "hello"
+    test()
